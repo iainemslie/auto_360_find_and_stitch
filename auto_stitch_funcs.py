c30a6c9bc436991d83ab755180ba4046e1bf7353
emsliei <iainemslie@gmail.com>
2021-09-18 17:34:03 -0600

    Initial commit

---
+++ auto_stitch_funcs.py
@@ -0,0 +1,215 @@
+import os
+import tifffile
+import numpy as np
+import multiprocessing as mp
+from functools import partial
+
+class AutoStitchFunctions:
+    def __init__(self, parameters):
+        self.lvl0 = os.path.abspath(parameters["input_dir"])
+        self.ct_dirs = []
+        self.ct_list = []
+        self.z_dirs = {}
+        self.overlap_range = int(parameters['overlap_end']) - int(parameters['overlap_start'])
+        self.parameters = parameters
+
+    def run_auto_stitch(self):
+        """
+        Main function that calls all other functions
+        """
+        self.print_parameters()
+
+        # Check input directory and find structure
+        print("--> Finding CT Directories")
+        self.find_ct_dirs()
+
+        # Get the names of the CT "sample" directories
+        self.get_ct_list()
+        print(self.ct_list)
+
+        # Create a dict with each CTDir as a key and a list of its subdirectories as the value
+        self.get_z_dirs()
+        print(self.z_dirs)
+
+        # Create the temp directory and internal structure
+        self.create_temp_dir()
+
+        # Get the images at 0 degrees and 180 degrees and stitch together the images
+        self.find_and_stich_images()
+        print("--> Finished Stitching")
+
+        # Do flat field correction for sli-0 and sli-180 for each possible range value
+
+    def find_ct_dirs(self):
+        """
+        Walks directories rooted at "Input Directory" location
+        Appends their absolute path to ctdir if they contain a directory with same name as "tomo" entry in GUI
+        """
+        for root, dirs, files in os.walk(self.lvl0):
+            for name in dirs:
+                if name == "tomo":
+                    self.ct_dirs.append(root)
+        self.ct_dirs = list(set(self.ct_dirs))
+
+    def get_ct_list(self):
+        """
+        Creates a list containing the "sample" or CT directory names
+        """
+        if len(self.ct_dirs) == 0:
+            print("--> No valid CT Directories found - Please select a different input directory")
+        else:
+            for path in self.ct_dirs:
+                ct_path, z_dir = os.path.split(path)
+                parent_path, ct_name = os.path.split(ct_path)
+                self.ct_list.append(ct_name)
+            self.ct_list = list(set(self.ct_list))
+
+    def get_z_dirs(self):
+        """
+        Creates a dictionary where each key is a CTDir and its value is a list of its subdirectories
+        """
+        for ct_dir in self.ct_list:
+            zdir_list = os.listdir(self.parameters['input_dir'] + "/" + ct_dir)
+            self.z_dirs[ct_dir] = zdir_list
+
+    def create_temp_dir(self):
+        """
+        Creates the temp directory and its subdirectories
+        The range directory contains directories named in sequence
+        from the start of the horizontal overlap range until the end
+        """
+        try:
+            os.mkdir(self.parameters['temp_dir'])
+            print("--> Creating Temp Directory: " + self.parameters['temp_dir'])
+            ct_items = self.z_dirs.items()
+            for ct_dir in ct_items:
+                os.makedirs(os.path.join(self.parameters['temp_dir'], ct_dir[0]))
+                for z_dir in ct_dir[1]:
+                    os.makedirs(os.path.join(self.parameters['temp_dir'], ct_dir[0], z_dir, "projection"))
+                    os.makedirs(os.path.join(self.parameters['temp_dir'], ct_dir[0], z_dir, "range"))
+                    for num in range(self.overlap_range + 1):
+                        tmp_path = os.path.join(self.parameters['temp_dir'], ct_dir[0],
+                                                z_dir, "range", str(int(self.parameters['overlap_start']) + num))
+                        os.makedirs(os.path.join(tmp_path, "tomo"))
+                        os.makedirs(os.path.join(tmp_path, "darks"))
+                        os.makedirs(os.path.join(tmp_path, "flats"))
+        except FileExistsError:
+            print("--> Directory " + self.parameters['temp_dir'] +
+                  " already exists - select a different temp directory or delete the current one")
+
+    def find_and_stich_images(self):
+        """
+        Gets the images corresponding to 0 degrees and 180 degrees.
+        In a dataset of 6000 images - we want 0 and 3000 stitched for 0 degrees - and 3001 and 6000 for 180 degrees
+        Also gets the full list of flats/darks and stitches those in pairs
+        """
+        ct_items = self.z_dirs.items()
+        for ct_dir in ct_items:
+            for zdir in ct_dir[1]:
+                # Get list of image names in the directory
+                image_list = sorted(os.listdir(os.path.join(self.parameters['input_dir'], ct_dir[0], zdir, "tomo")))
+                num_images = len(image_list)
+                # Get the images corresponding to 0 and 180 degree rotations in half-acquisition mode
+                first_zero_degree_image = image_list[0]
+                second_zero_degree_image = image_list[int(num_images/2)]
+                first_180_degree_image = image_list[int((num_images/2)+1)]
+                second_180_degree_image = image_list[num_images-1]
+                # Get absolute paths for these images
+                tmp_path = os.path.join(self.parameters['input_dir'], ct_dir[0], zdir, "tomo")
+                first_zero_degree_image_path = os.path.join(tmp_path, first_zero_degree_image)
+                second_zero_degree_image_path = os.path.join(tmp_path, second_zero_degree_image)
+                first_180_degree_image_path = os.path.join(tmp_path, first_180_degree_image)
+                second_180_degree_image_path = os.path.join(tmp_path, second_180_degree_image)
+                # Get the list of images in flats, darks
+                flats_list = sorted(os.listdir(os.path.join(self.parameters['input_dir'], ct_dir[0], zdir, "flats")))
+                tmp_flat_path = os.path.join(self.parameters['input_dir'], ct_dir[0], zdir, "flats")
+                darks_list = sorted(os.listdir(os.path.join(self.parameters['input_dir'], ct_dir[0], zdir, "darks")))
+                tmp_dark_path = os.path.join(self.parameters['input_dir'], ct_dir[0], zdir, "darks")
+                # For each axis value in overlap range we stitch corresponding images and save to temp directory
+                pool = mp.Pool(processes=mp.cpu_count())
+                index = range(self.overlap_range + 1)
+                exec_func = partial(self.stitch_fdt, ct_dir, zdir, first_zero_degree_image_path, second_zero_degree_image_path,
+                                    first_180_degree_image_path, second_180_degree_image_path,
+                                    flats_list, tmp_flat_path, darks_list, tmp_dark_path)
+                pool.map(exec_func, index)
+
+    def stitch_fdt(self, ct_dir, zdir, first_zero_degree_image_path, second_zero_degree_image_path,
+                   first_180_degree_image_path, second_180_degree_image_path, flats_list, tmp_flat_path,
+                   darks_list, tmp_dark_path, index):
+        """
+        Stitches together the input flats/darks/tomo images and outputs to temp directory
+        """
+        rotation_axis = int(self.parameters['overlap_start']) + index
+        out_path = os.path.join(self.parameters['temp_dir'], ct_dir[0],
+                                zdir, "range", str(rotation_axis))
+        slice_zero_path = os.path.join(out_path, "tomo", "Sli-0")
+        slice_180_path = os.path.join(out_path, "tomo", "Sli-180")
+        self.open_images_and_stitch(rotation_axis, 0, first_zero_degree_image_path,
+                                    second_zero_degree_image_path, slice_zero_path)
+        self.open_images_and_stitch(rotation_axis, 0, first_180_degree_image_path,
+                                    second_180_degree_image_path, slice_180_path)
+        # Stitch pairs of images together - for 20 images we stitch 0-10, 1-11, ..., 9-19
+        flat_midpoint = int(len(flats_list) / 2)
+        for flat_index in range(flat_midpoint):
+            first_flat_path = os.path.join(tmp_flat_path, flats_list[flat_index])
+            second_flat_path = os.path.join(tmp_flat_path, flats_list[flat_index + flat_midpoint])
+            flat_out_path = os.path.join(out_path, "flats", "Flat_stitched_{:>04}".format(flat_index))
+            self.open_images_and_stitch(rotation_axis, 0, first_flat_path, second_flat_path, flat_out_path)
+        dark_midpoint = int(len(darks_list) / 2)
+        for dark_index in range(dark_midpoint):
+            first_dark_path = os.path.join(tmp_dark_path, darks_list[dark_index])
+            second_dark_path = os.path.join(tmp_dark_path, darks_list[dark_index + dark_midpoint])
+            dark_out_path = os.path.join(out_path, "darks", "Dark_stitched_{:>04}".format(dark_index))
+            self.open_images_and_stitch(rotation_axis, 0, first_dark_path, second_dark_path, dark_out_path)
+
+    def print_parameters(self):
+        """
+        Prints parameter values with line formatting
+        """
+        print("**************************** Running Auto Stitch ****************************")
+        print("======================== Parameters ========================")
+        print("Input Directory: " + self.parameters['input_dir'])
+        print("Output Directory: " + self.parameters['output_dir'])
+        print("Temp Directory: " + self.parameters['temp_dir'])
+        print("Overlap Start: " + self.parameters['overlap_start'])
+        print("Overlap End: " + self.parameters['overlap_end'])
+        print("Number of Steps: " + self.parameters['steps'])
+        print("Axis on left: " + self.parameters['axis_on_left'])
+        print("============================================================")
+
+    """****** BORROWED FUNCTIONS ******"""
+
+    def read_image(self, file_name):
+        """Read tiff file from disk by :py:mod:`tifffile` module."""
+        with tifffile.TiffFile(file_name) as f:
+            return f.asarray(out='memmap')
+
+    def open_images_and_stitch(self, ax, crop, first_image_path, second_image_path, out_fmt):
+        # we pass index and formats as argument
+        first = self.read_image(first_image_path)
+        second = self.read_image(second_image_path)
+        stitched = self.stitch(first, second, ax, crop)
+        tifffile.imsave(out_fmt, stitched)
+
+    def stitch(self, first, second, axis, crop):
+        h, w = first.shape
+        if axis > w / 2:
+            dx = int(2 * (w - axis) + 0.5)
+        else:
+            dx = int(2 * axis + 0.5)
+            tmp = np.copy(first)
+            first = second
+            second = tmp
+        result = np.empty((h, 2 * w - dx), dtype=first.dtype)
+        ramp = np.linspace(0, 1, dx)
+
+        # Mean values of the overlapping regions must match, which corrects flat-field inconsistency
+        # between the two projections
+        k = np.mean(first[:, w - dx:]) / np.mean(second[:, :dx])
+        second = second * k
+
+        result[:, :w - dx] = first[:, :w - dx]
+        result[:, w - dx:w] = first[:, w - dx:] * (1 - ramp) + second[:, :dx] * ramp
+        result[:, w:] = second[:, dx:]
+
+        return result[:, slice(int(crop), int(2 * (w - axis) - crop), 1)]
---
+++ auto_stitch_gui.py
@@ -0,0 +1,183 @@
+import os
+import sys
+import logging
+import shutil
+
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLineEdit, QGridLayout, QFileDialog, QCheckBox,\
+                            QMessageBox
+from auto_stitch_funcs import AutoStitchFunctions
+
+
+class AutoStitchGUI(QWidget):
+
+    def __init__(self, *args, **kwargs):
+        super(AutoStitchGUI, self).__init__(*args, **kwargs)
+        self.setWindowTitle('Auto Stitch')
+
+        logger = logging.getLogger()
+        logger.setLevel(logging.DEBUG)
+
+        self.parameters = {}
+        self.auto_stitch_funcs = None
+
+        self.input_button = QPushButton("Select Input Path")
+        self.input_button.clicked.connect(self.input_button_pressed)
+        self.input_entry = QLineEdit()
+        self.input_entry.textChanged.connect(self.set_input_entry)
+
+        self.output_button = QPushButton("Select Output Path")
+        self.output_button.clicked.connect(self.output_button_pressed)
+        self.output_entry = QLineEdit()
+        self.output_entry.textChanged.connect(self.set_output_entry)
+
+        self.temp_button = QPushButton("Select Temp Path")
+        self.temp_button.clicked.connect(self.temp_button_pressed)
+        self.temp_entry = QLineEdit()
+        self.temp_entry.textChanged.connect(self.set_temp_entry)
+
+        self.overlap_start_label = QLabel("Starting horizontal overlap")
+        self.overlap_start_entry = QLineEdit()
+        self.overlap_start_entry.textChanged.connect(self.set_overlap_start_entry)
+
+        self.overlap_end_label = QLabel("Ending horizontal overlap")
+        self.overlap_end_entry = QLineEdit()
+        self.overlap_end_entry.textChanged.connect(self.set_overlap_end_entry)
+
+        self.steps_label = QLabel("Number of steps")
+        self.steps_entry = QLineEdit()
+        self.steps_entry.textChanged.connect(self.set_steps_entry)
+
+        self.left_hand_checkbox = QCheckBox("Is the rotation axis on the left-hand side of the image?")
+        self.left_hand_checkbox.stateChanged.connect(self.set_left_hand_checkbox)
+
+        self.help_button = QPushButton("Help")
+        self.help_button.clicked.connect(self.help_button_pressed)
+
+        self.delete_temp_button = QPushButton("Delete Temp Directory")
+        self.delete_temp_button.clicked.connect(self.delete_button_pressed)
+
+        self.stitch_button = QPushButton("Stitch")
+        self.stitch_button.clicked.connect(self.stitch_button_pressed)
+
+        self.set_layout()
+        self.resize(800, 0)
+
+        self.init_values()
+        self.show()
+
+    def set_layout(self):
+        layout = QGridLayout()
+        layout.addWidget(self.input_button, 0, 0, 1, 2)
+        layout.addWidget(self.input_entry, 0, 2, 1, 4)
+        layout.addWidget(self.output_button, 1, 0, 1, 2)
+        layout.addWidget(self.output_entry, 1, 2, 1, 4)
+        layout.addWidget(self.temp_button, 2, 0, 1, 2)
+        layout.addWidget(self.temp_entry, 2, 2, 1, 4)
+        layout.addWidget(self.overlap_start_label, 3, 0)
+        layout.addWidget(self.overlap_start_entry, 3, 1)
+        layout.addWidget(self.overlap_end_label, 3, 2)
+        layout.addWidget(self.overlap_end_entry, 3, 3)
+        layout.addWidget(self.steps_label, 3, 4)
+        layout.addWidget(self.steps_entry, 3, 5)
+        layout.addWidget(self.left_hand_checkbox, 4, 0, 1, 4)
+        layout.addWidget(self.help_button, 5, 0, 1, 2)
+        layout.addWidget(self.delete_temp_button, 5, 2, 1, 1)
+        layout.addWidget(self.stitch_button, 5, 3, 1, 3)
+        self.setLayout(layout)
+
+    def init_values(self):
+        working_dir = os.getcwd()
+        self.input_entry.setText(working_dir)
+        self.parameters['input_dir'] = working_dir
+        output_dir = working_dir + "/rec/horstitch"
+        self.output_entry.setText(output_dir)
+        self.parameters['output_dir'] = output_dir
+        temp_dir = "/data/tmp-auto-stitch"
+        self.temp_entry.setText(temp_dir)
+        self.parameters['temp_dir'] = temp_dir
+        self.overlap_start_entry.setText("720")
+        self.parameters['overlap_start'] = "720"
+        self.overlap_end_entry.setText("735")
+        self.parameters['overlap_end'] = "735"
+        self.steps_entry.setText("1")
+        self.parameters['steps'] = "1"
+        self.left_hand_checkbox.setChecked(False)
+        self.parameters['axis_on_left'] = str(False)
+
+    def input_button_pressed(self):
+        logging.debug("Input Button Pressed")
+        dir_explore = QFileDialog(self)
+        input_dir = dir_explore.getExistingDirectory()
+        self.input_entry.setText(input_dir)
+        self.parameters['input_dir'] = input_dir
+        self.output_entry.setText(input_dir + "/rec/horstitch")
+        self.parameters['output_dir'] = input_dir + "/rec/horstitch"
+
+    def set_input_entry(self):
+        logging.debug("Input Entry: " + str(self.input_entry.text()))
+        self.parameters['input_dir'] = str(self.input_entry.text())
+
+    def output_button_pressed(self):
+        logging.debug("Output Button Pressed")
+        dir_explore = QFileDialog(self)
+        output_dir = dir_explore.getExistingDirectory()
+        self.output_entry.setText(output_dir)
+        self.parameters['output_dir'] = output_dir
+
+    def set_output_entry(self):
+        logging.debug("Output Entry: " + str(self.output_entry.text()))
+        self.parameters['output_dir'] = str(self.output_entry.text())
+
+    def temp_button_pressed(self):
+        logging.debug("Temp Button Pressed")
+        dir_explore = QFileDialog(self)
+        temp_dir = dir_explore.getExistingDirectory()
+        self.temp_entry.setText(temp_dir)
+        self.parameters['temp_dir'] = temp_dir
+
+    def set_temp_entry(self):
+        logging.debug("Temp Entry: " + str(self.temp_entry.text()))
+        self.parameters['temp_dir'] = str(self.temp_entry.text())
+
+    def set_overlap_start_entry(self):
+        logging.debug("Overlap Start: " + str(self.overlap_start_entry.text()))
+        self.parameters['overlap_start'] = str(self.overlap_start_entry.text())
+
+    def set_overlap_end_entry(self):
+        logging.debug("Overlap End: " + str(self.overlap_end_entry.text()))
+        self.parameters['overlap_end'] = str(self.overlap_end_entry.text())
+
+    def set_steps_entry(self):
+        logging.debug("Steps: " + str(self.steps_entry.text()))
+        self.parameters['steps'] = str(self.steps_entry.text())
+
+    def set_left_hand_checkbox(self):
+        logging.debug("Rotation axis on left-hand-side checkbox: " + str(self.left_hand_checkbox.isChecked()))
+        self.parameters['axis_on_left'] = str(self.left_hand_checkbox.isChecked())
+
+    def help_button_pressed(self):
+        logging.debug("Help Button Pressed")
+        h = "Lorem Ipsum\n"
+        QMessageBox.information(self, "Help", h)
+
+    def delete_button_pressed(self):
+        logging.debug("Delete Temp Directory Button Pressed")
+        delete_dialog = QMessageBox.question(self, 'Quit', 'Are you sure you want to delete the temporary directory?',
+                                             QMessageBox.Yes | QMessageBox.No)
+        if delete_dialog == QMessageBox.Yes:
+            try:
+                shutil.rmtree(self.parameters['temp_dir'])
+                print("Deleted directory: " + self.parameters['temp_dir'])
+            except FileNotFoundError:
+                print("Directory does not exist: " + self.parameters['temp_dir'])
+
+    def stitch_button_pressed(self):
+        logging.debug("Stitch Button Pressed")
+        self.auto_stitch_funcs = AutoStitchFunctions(self.parameters)
+        self.auto_stitch_funcs.run_auto_stitch()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = AutoStitchGUI()
+    sys.exit(app.exec_())
+
