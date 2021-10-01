728c955e7e0104b14e0a03ea8e2a33bd5b5354f5
emsliei <iainemslie@gmail.com>
2021-10-01 10:22:08 -0600

    Begin updating with new FFTConvolve idea

--- auto_stitch_funcs.py
+++ auto_stitch_funcs.py
@@ -120,7 +120,7 @@ class AutoStitchFunctions:
         # We must crop the first image from first pixel column up until overlap
         first_cropped = first[:, :int(self.parameters['overlap_region'])]
         # We must crop the 180 degree rotation (which has been flipped 180) from width-overlap until last pixel column
-        second_cropped = second[:, int(width - self.parameters['overlap_region']):]
+        second_cropped = second[:, int(width - int(self.parameters['overlap_region'])):]
 
         axis = self.compute_rotation_axis(first, second)
         print("axis: ", end="")
